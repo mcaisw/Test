2c3c7668b51186eee07441ff54dd59968d899e15
MengChi <mc2311362372@gmail.com>
2018-03-07 19:18:40 +0800

    ··

--- Assets/Script/GameInput.cs
+++ Assets/Script/GameInput.cs
@@ -4,21 +4,45 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour {
 
-    float gapTime=0.3f;
+    float   gapTime=0.3f;
+    bool    doubleHit = false; 
+    int     tapTimes;
+    float   elapsedTimes;
+    bool    countTimes;
+    bool    quickClick;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        quickClick = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetMouseButton(0))
-        {
-            Debug.Log("jump");
-            Player.Instance.Jump();
+        if (Input.GetMouseButtonDown(0)&& gapTime>0) {
+            tapTimes++;
+            countTimes = true;
         }
-		
-	}
+        
+        if (countTimes&&gapTime > 0)
+        {
+            gapTime -= Time.deltaTime;
+        }
+
+        if (gapTime<=0)
+        {
+            countTimes = false;
+        }
+
+    }
+
+    private void LateUpdate()
+    {
+        if (gapTime<=0)
+        {
+            Debug.Log(tapTimes);
+            tapTimes = 0;
+            gapTime = 0.3f;
+        }
+    }
 }
